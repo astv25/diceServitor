db3476223e3433e6a8d134d7e1565e82f00cfd19
themerc14 <jthaerle04@gmail.com>
2021-07-08 23:36:46 -0700

    Update DiceBot3.py
    its done

--- DiceBot3.py
+++ DiceBot3.py
@@ -56,10 +56,10 @@ def diceRolling(input):
                     dropped.append(rolls[0])
                     rolls.pop(0)
                     index+=1
-                total=sum(rolls)
-                out.append("Rolled {0}.  Dropped {1}.  Total {2}".format(origrolls,dropped,total))
+                #total=sum(rolls)
+                #out.append("Rolled {0}.  Dropped {1}.  Total {2}".format(origrolls,dropped,total))
                 #print(out)
-                return out
+                return rolls
             if mod == '>' or mod == '>=':
                 if '=' in mod:
                     compequal=True
@@ -138,25 +138,68 @@ def diceRolling(input):
             return "Are you sure?"
         def four():
             results = rolling(int(matches[0]),int(matches[1]))
+            dropTotal = 0
             if (matches[2][0] in modSymbols):
                 mod = matches[2]                
-            else:
-                return "Bad stop it"   
+            if (matches[2][0] in mathSymbols):
+                resultsTotal = 0
+                for x in results:
+                    resultsTotal += x
+                resultsTotal += mathHandling(matches[2])
+                return str(results) + " Total: " + str(resultsTotal) +" "+ matches[3]
             if (matches[3].isdigit()):
                 modNum = matches[3]
                 modNum = int(modNum)
             else:
                 return "Reconsider this entry... and your life choices"
-            return compareOrDrop(results,mod,modNum)
-
-            
-
-
-            #return "this is a roll with a single mod"
+            results = compareOrDrop(results,mod,modNum)
+            if(matches[2][0] == 'd'):
+                for x in results:
+                    dropTotal += int(x)
+                return str(results) + " Total: " + str(dropTotal)
+            return results    
         def five():
-            return "this is a die roll with multiple mods and/or a comment"
+            results = rolling(int(matches[0]),int(matches[1]))
+            if (matches[2][0] in modSymbols and matches[3][0].isdigit()):
+                mod = matches[2]
+                modNum = int(matches[3])
+            else:
+                return "Something ain't right chief"
+            if('#' in matches[4]):
+                output = compareOrDrop(results,mod,modNum)
+                return str(output) + matches[4]
+            if (matches[4][0] in mathSymbols):
+                output = compareOrDrop(results,mod,modNum)
+                if matches[2][0] == 'd':
+                    resultsTotal = 0
+                    for x in output:
+                        resultsTotal += x
+                    resultsTotal += mathHandling(matches[4])
+                else:
+                    resultsTotal = 0
+                    for x in results:
+                        resultsTotal += x
+                    resultsTotal += mathHandling(matches[4])   
+
+                return str(output) + "Total = " + str(resultsTotal)
+
+            return "Literally How?"
         def six():
-            return "This is a die roll with multiple mods and a comment"
+            results = rolling(int(matches[0]),int(matches[1]))
+            mod = matches[2]
+            modNum = int(matches[3])
+            output = compareOrDrop(results,mod,modNum)
+            if matches[2][0] == 'd': 
+                resultsTotal = 0
+                for x in output:
+                    resultsTotal += x
+                resultsTotal += mathHandling(matches[4])
+            else:
+                resultsTotal = 0
+                for x in results:
+                    resultsTotal += x
+                resultsTotal += mathHandling(matches[4])
+            return str(output) + "Total = " + str(resultsTotal) +" "+ matches[5]
 
         def groupCases(argument):
             switcher = {
@@ -175,12 +218,12 @@ def diceRolling(input):
         print(str(e))
     
 
-diceRolling("1d100>=45")
-diceRolling("1d100<=30")
-diceRolling("3d10dl2")
-diceRolling("1d100>45")
-diceRolling("1d100<30")
-# for test in testCases:
-#     diceRolling(test)
-#     print("End Roll")
+#diceRolling("3d10dl2+5+6#I am the comment")
+# diceRolling("1d100<=30")
+#diceRolling("3d10dl1")
+# diceRolling("1d100>45")
+#diceRolling("2d100<30")
+for test in testCases:
+    diceRolling(test)
+    print("End Roll")
 
